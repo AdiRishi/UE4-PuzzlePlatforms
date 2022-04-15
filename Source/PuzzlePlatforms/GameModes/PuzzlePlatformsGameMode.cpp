4457d23eb0cde98e70c4881d9638e005975730bb
Adishwar Rishi <adiswa123@gmail.com>
2022-04-16 01:07:57 +1000

    Refactor codebase

--- Source/PuzzlePlatforms/GameModes/PuzzlePlatformsGameMode.cpp
+++ Source/PuzzlePlatforms/GameModes/PuzzlePlatformsGameMode.cpp
@@ -1,7 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "PuzzlePlatformsGameMode.h"
-#include "PuzzlePlatformsCharacter.h"
+#include "PuzzlePlatforms/GameModes/PuzzlePlatformsGameMode.h"
+#include "PuzzlePlatforms/Characters/PuzzlePlatformsCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 APuzzlePlatformsGameMode::APuzzlePlatformsGameMode()
